81a3da7b429550e396a08fc2d66cd129f8cce0d8
Marlon Tamanini <marlon@inlift.com.br>
2026-05-05 16:27:53 -0300

    Ajusta conexão com DATABASE_URL

--- src/database/conexao.py
+++ src/database/conexao.py
@@ -5,10 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(
+            database_url,
+            sslmode="require",
+            connect_timeout=10
+        )
+
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        sslmode="require",
+        connect_timeout=10
     )
